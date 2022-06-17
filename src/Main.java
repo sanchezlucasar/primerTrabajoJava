f9c7dd80bedbb2559b6b6c2caca2f1fd66c0de01
sanchezlucasar <sanchezlucasar@gmail.com>
2022-06-17 17:37:01 -0300

    1erTrabajo

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=1;
+        int b=1;
+        int c=1;
+
+        int resultado = suma(a,b,c);
+        System.out.println(resultado);
+
+    }
+
+    public static int suma(int a, int b, int c){
+        return a+b+c;
+    }
+
+}
